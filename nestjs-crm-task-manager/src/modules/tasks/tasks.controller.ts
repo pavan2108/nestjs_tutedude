7a36bb75f221c145fd5889560e160badfa83b28a
Pavan Akurathi <pavansssai2001@gmail.com>
2026-07-31 00:28:19 +0530

    feat: added tasks controller

--- nestjs-crm-task-manager/src/modules/tasks/tasks.controller.ts
+++ nestjs-crm-task-manager/src/modules/tasks/tasks.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { GetTasksDto } from './dto/get-tasks.dto';
 import { TasksService } from './tasks.service';
 import { type AuthRequest } from '../../utils/types/auth.types';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { AssignTaskDto } from './dto/assign-task.dto';
 
 @ApiBearerAuth()
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  async create(@Body() dto: CreateTaskDto) {
+    return this.tasksService.create(dto);
+  }
+
+  @Put('assign')
+  async assign(@Body() dto: AssignTaskDto) {
+    return this.tasksService.assign(dto);
+  }
+
+  @Put(':task_id')
+  async update(
+    @Param('task_id', ParseIntPipe) task_id: number,
+    dto: UpdateTaskDto,
+  ) {
+    return this.tasksService.update(task_id, dto);
+  }
 
   @Get()
   get(@Req() req: AuthRequest, @Query() dto: GetTasksDto) {
@@ -16,5 +48,10 @@ export class TasksController {
       userId: req.user.id,
       role: req.user.role,
     });
+  }
+
+  @Delete(':task_id')
+  async delete(@Param('task_id', ParseIntPipe) task_id: number) {
+    return this.tasksService.delete(task_id);
   }
 }
